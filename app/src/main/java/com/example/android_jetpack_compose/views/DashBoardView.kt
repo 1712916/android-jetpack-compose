02f6fc13fb87973c776164096795471a1bbd4937
1712916 <kieuphong2840@gmail.com>
2023-08-18 00:10:47 +0700

    add action for top bar
    #add icon svg

--- app/src/main/java/com/example/android_jetpack_compose/views/DashBoardView.kt
+++ app/src/main/java/com/example/android_jetpack_compose/views/DashBoardView.kt
@@ -1,11 +1,13 @@
 package com.example.android_jetpack_compose.views
 
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 
@@ -13,7 +15,12 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.outlined.Notifications
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 
 
 import androidx.compose.material3.Scaffold
@@ -24,20 +31,44 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.android_jetpack_compose.R
 import com.example.android_jetpack_compose.ui.theme.AndroidjetpackcomposeTheme
 
-val primaryColor = Color(0xFF081f68);
-val grayColor = Color(0xFFd9dae3);
+val primaryColor = Color(0xFF081f68)
+val grayColor = Color(0xFFd9dae3)
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DashBoardView() {
+    val contextForToast = LocalContext.current
+
     Scaffold(
         topBar = {
-            AppBar(title = "Overview")
+            AppBar(title = "Overview",
+                actions = {
+                    IconButton(onClick =
+                    {
+                        Toast.makeText(
+                            contextForToast,
+                            "This function is coming soon!",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }) {
+                        Icon(
+//                            imageVector = Icons.Filled.Notifications,
+                            imageVector = ImageVector.vectorResource(id = R.drawable.ic_notification),
+                            contentDescription = "Notifications",
+                            modifier = Modifier.width(24.dp).height(24.dp)
+                        )
+                    }
+                }
+            )
         },
         content = { contentPadding ->
 
@@ -79,7 +110,7 @@ private fun TrackingColum(height: Double? = null, width: Double? = null, percent
     val trackColumnWidth: Double = 40.0
     val finalHeight = height ?: trackColumnHeight
     val finalWidth = width ?: trackColumnWidth
-    val borderRadius = 8.0;
+    val borderRadius = 8.0
     Box(
         modifier = Modifier
             .background(color = grayColor, shape = RoundedCornerShape(borderRadius.dp))
@@ -113,7 +144,7 @@ fun SimpleColumn() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AppBar(title: String) {
+fun AppBar(title: String, actions: @Composable RowScope.() -> Unit = {}) {
 
     // TopAppBar Composable
     TopAppBar(
@@ -125,8 +156,8 @@ fun AppBar(title: String) {
 
                 )
         },
-
-        )
+        actions = actions
+    )
 }
 
 @Preview(showBackground = true)
