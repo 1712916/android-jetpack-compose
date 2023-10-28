51cb63743af611c04e16945d02889cca3564e22a
1712916 <kieuphong2840@gmail.com>
2023-10-28 10:31:13 +0700

    add flow (wrap) multi selection view

--- app/src/main/java/com/example/android_jetpack_compose/ui/daily_expense/InputDailyExpenseView.kt
+++ app/src/main/java/com/example/android_jetpack_compose/ui/daily_expense/InputDailyExpenseView.kt
@@ -1,18 +1,11 @@
 package com.example.android_jetpack_compose.ui.daily_expense
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.FilterChip
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.material.icons.filled.List
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.*
+
+import androidx.compose.material.*
+import androidx.compose.material.icons.*
+import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -24,156 +17,188 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.android_jetpack_compose.ui.dashboard.HeightBox
 import com.example.android_jetpack_compose.ui.dashboard.WidthBox
-import com.example.android_jetpack_compose.ui.view.AppNumberBoard
-import com.example.android_jetpack_compose.ui.view.MultipleSelectionHorizontalView
+import com.example.android_jetpack_compose.ui.view.*
+import kotlinx.coroutines.launch
 
 @Preview
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterialApi::class)
 @Composable
 fun InputDailyExpenseView() {
     var text by remember { mutableStateOf("Hello") }
+    val scope = rememberCoroutineScope()
+    val bottomSheetState = rememberModalBottomSheetState(
+        initialValue = ModalBottomSheetValue.Hidden
+    )
 
-    Scaffold { p ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(p),
-//            verticalArrangement =    Arrangement.SpaceBetween
-        ) {
-            LazyColumn(
+    ModalBottomSheetLayout(
+        sheetState = bottomSheetState,
+        sheetContent = {
+            Text("BottomSheet")
+            Text("BottomSheet")
+            Text("BottomSheet")
+            Text("BottomSheet")
+            Text("BottomSheet")
+            Text("BottomSheet")
+            Text("BottomSheet")
+            Text("BottomSheet")
+            Text("BottomSheet")
+        }
+    ) {
+        Scaffold { p ->
+            Column(
                 modifier = Modifier
-                    .weight(1f)
-                    .padding(16.dp),
-                content = {
-                    item {
-                        MoneyInputView(
-                            number = text,
-                            validateState = InputMoneyValidator(
-                                source = "",
-                                destination = text
-                            ).validate()
-                        )
-                    }
-                    item {
-                        HeightBox(height = 16.0)
-                    }
-                    item {
-                        Column {
-                            Text("Note:")
-                            HeightBox(height = 8.0)
-                            OutlinedTextField(
-                                value = text, onValueChange = { text = it },
-                                modifier = Modifier.fillMaxWidth()
+                    .fillMaxSize()
+                    .padding(p),
+//            verticalArrangement =    Arrangement.SpaceBetween
+            ) {
+                LazyColumn(
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(16.dp),
+                    content = {
+                        item {
+                            MoneyInputView(
+                                number = text,
+                                validateState = InputMoneyValidator(
+                                    source = "",
+                                    destination = text
+                                ).validate()
                             )
                         }
-                    }
-                    item {
-                        HeightBox(height = 16.0)
-                    }
-                    item {
-                        Column(modifier = Modifier.padding(vertical = 16.dp)) {
-                            Text("Categories")
-                            Row {
-                                LazyRow(
-                                    modifier = Modifier.weight(1f),
-                                    content = {
-                                        item {
-                                            FilterChip(
-                                                onClick = { },
-                                                selected = false,
-                                                content = {
-                                                    Icon(
-                                                        imageVector = Icons.Default.AccountBox,
-                                                        contentDescription = ""
-                                                    )
-                                                }
-                                            )
-                                            WidthBox(width = 8.0)
-                                        }
-                                        items(count = 10) { index ->
-                                            FilterChip(
-                                                onClick = { },
-                                                selected = false,
-                                                leadingIcon = {
-                                                    Icon(
-                                                        imageVector = Icons.Default.AccountBox,
-                                                        contentDescription = ""
-                                                    )
-                                                },
-                                                content = {
-                                                    Text(index.toString())
-                                                }
-                                            )
-                                            WidthBox(width = 8.0)
-                                        }
-                                    })
-                                IconButton(onClick = { /*TODO*/ }) {
-                                    Icon(
-                                        imageVector = Icons.Default.List,
-                                        contentDescription = "See All"
-                                    )
-                                }
+                        item {
+                            HeightBox(height = 16.0)
+                        }
+                        item {
+                            Column {
+                                Text("Note:")
+                                HeightBox(height = 8.0)
+                                OutlinedTextField(
+                                    value = text, onValueChange = { text = it },
+                                    modifier = Modifier.fillMaxWidth()
+                                )
                             }
                         }
-                    }
-                    item {
-                        Column {
-                            Text("Methods")
-                            Row {
-                                Box(modifier = Modifier.weight(1f)) {
-                                    MultipleSelectionHorizontalView<Int>(
-                                        data = arrayListOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
-                                        selectedList = arrayListOf(2),
-                                        onChange = {
-                                                   print(it.toString())
-                                        },
-                                        itemBuilder = { item, isSelected ->
-                                            Row {
-                                                FilterChip(
-                                                    onClick = { },
-                                                    selected = isSelected,
-                                                    leadingIcon = {
-                                                        Icon(
-                                                            imageVector = Icons.Default.AccountBox,
-                                                            contentDescription = ""
-                                                        )
-                                                    },
-                                                    content = {
-                                                        Text(item.toString())
-                                                    }
-                                                )
-                                                WidthBox(width = 8.0)
+                        item {
+                            HeightBox(height = 16.0)
+                        }
+                        item {
+                            Column(modifier = Modifier.padding(vertical = 16.dp)) {
+                                Row(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    horizontalArrangement = Arrangement.SpaceBetween,
+                                    verticalAlignment = Alignment.CenterVertically
+                                    ) {
+                                    Text("Categories")
+                                    IconButton(onClick = { /*TODO*/ }) {
+                                        Icon(
+                                            imageVector = Icons.Default.Add,
+                                            contentDescription = "See All"
+                                        )
+                                    }
+                                }
+                                Row {
+                                    Box(modifier = Modifier.weight(1f)) {
+                                        MultipleSelectionFlowView<Int>(
+                                            data = arrayListOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
+                                            selectedList = arrayListOf(2),
+                                            onChange = {
+                                                print(it.toString())
+                                            },
+                                            itemBuilder = { item, isSelected ->
+                                                Row {
+                                                    FilterChip(
+                                                        onClick = { },
+                                                        selected = isSelected,
+                                                        leadingIcon = {
+                                                            Icon(
+                                                                imageVector = Icons.Default.AccountBox,
+                                                                contentDescription = ""
+                                                            )
+                                                        },
+                                                        content = {
+                                                            Text(item.toString())
+                                                        }
+                                                    )
+                                                    WidthBox(width = 8.0)
+                                                }
                                             }
-                                        }
-                                    )
-                                }
-
-                                IconButton(onClick = { /*TODO*/ }) {
-                                    Icon(
-                                        imageVector = Icons.Default.List,
-                                        contentDescription = "See All"
-                                    )
+                                        )
+                                    }
                                 }
                             }
                         }
-                    }
-                },
-            )
-            Button(
-                onClick = { },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp)
-            ) {
-                Text("Save")
+                        item {
+                            Column {
+                                Row(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    horizontalArrangement = Arrangement.SpaceBetween,
+                                    verticalAlignment = Alignment.CenterVertically
+                                ) {
+                                    Text("Methods")
+                                    IconButton(onClick = { /*TODO*/ }) {
+                                        Icon(
+                                            imageVector = Icons.Default.List,
+                                            contentDescription = "See All"
+                                        )
+                                    }
+                                }
+                                Row {
+                                    Box(modifier = Modifier.weight(1f)) {
+                                        MultipleSelectionFlowView<Int>(
+                                            data = arrayListOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
+                                            selectedList = arrayListOf(2),
+                                            onChange = {
+                                                print(it.toString())
+                                            },
+                                            itemBuilder = { item, isSelected ->
+                                                Row {
+                                                    FilterChip(
+                                                        onClick = { },
+                                                        selected = isSelected,
+                                                        leadingIcon = {
+                                                            Icon(
+                                                                imageVector = Icons.Default.AccountBox,
+                                                                contentDescription = ""
+                                                            )
+                                                        },
+                                                        content = {
+                                                            Text(item.toString())
+                                                        }
+                                                    )
+                                                    WidthBox(width = 8.0)
+                                                }
+                                            }
+                                        )
+                                    }
+                                }
+                            }
+                        }
+                    },
+                )
+                Button(
+                    onClick = {
+                        scope.launch {
+                            scope.launch {
+                                bottomSheetState.show()
+                            }
+                        }
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                ) {
+                    Text("Save")
+                }
+                AppNumberBoard(onChanged = { text = it }, text = text)
             }
-            AppNumberBoard(onChanged = { text = it }, text = text)
         }
     }
 }
--- app/src/main/java/com/example/android_jetpack_compose/ui/view/MultipleSelectionView.kt
+++ app/src/main/java/com/example/android_jetpack_compose/ui/view/MultipleSelectionView.kt
@@ -2,10 +2,14 @@ package com.example.android_jetpack_compose.ui.view
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun <T> MultipleSelectionHorizontalView(
@@ -33,4 +37,34 @@ fun <T> MultipleSelectionHorizontalView(
             }
         }
     })
+}
+
+@OptIn(ExperimentalLayoutApi::class)
+@Composable
+fun <T> MultipleSelectionFlowView(
+    data: Iterable<T>,
+    selectedList: Iterable<T>,
+    onChange: (List<T>) -> Unit,
+    itemBuilder:@Composable (T, Boolean) -> Unit,
+) {
+    val selectedSet = selectedList.toSet()
+    val dataList = data.toList()
+    FlowRow(modifier = Modifier.padding(8.dp)) {
+        repeat(data.count()) {
+            val item = dataList[it]
+            Box(modifier = Modifier.clickable {
+                onChange(
+                    selectedSet.toMutableList().apply {
+                        if (selectedSet.contains(item)) {
+                            remove(item)
+                        } else {
+                            add(item)
+                        }
+                    }.toList(),
+                )
+            }) {
+                itemBuilder(item, selectedSet.contains(item))
+            }
+        }
+    }
 }
