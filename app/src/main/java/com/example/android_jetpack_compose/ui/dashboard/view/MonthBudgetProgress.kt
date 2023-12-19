9206a9254a6d3fabe350c69d664e5595de614245
1712916 <kieuphong2840@gmail.com>
2023-12-19 23:04:41 +0700

    reload current total month expense

--- app/src/main/java/com/example/android_jetpack_compose/ui/dashboard/view/MonthBudgetProgress.kt
+++ app/src/main/java/com/example/android_jetpack_compose/ui/dashboard/view/MonthBudgetProgress.kt
@@ -8,18 +8,38 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.*
 import androidx.compose.ui.draw.*
 import androidx.compose.ui.graphics.*
+import androidx.compose.ui.platform.*
 import androidx.compose.ui.text.*
 import androidx.compose.ui.text.font.*
 import androidx.compose.ui.unit.*
+import androidx.lifecycle.*
+import androidx.lifecycle.viewmodel.compose.*
 import com.example.android_jetpack_compose.ui.dashboard.view_model.*
 import com.example.android_jetpack_compose.ui.theme.*
 import com.example.android_jetpack_compose.ui.view.*
-import androidx.lifecycle.viewmodel.compose.*
 import com.example.android_jetpack_compose.util.*
 
 @Composable
 fun MonthBudgetProgress(viewModel: MonthProgressInfoViewModel = viewModel()) {
     val monthTackerInfoState by viewModel.monthTackerInfoState.collectAsState()
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val lifecycleState by lifecycleOwner.lifecycle.currentStateFlow.collectAsState()
+
+    LaunchedEffect(lifecycleState) {
+        // Do something with your state
+        // You may want to use DisposableEffect or other alternatives
+        // instead of LaunchedEffect
+        when (lifecycleState) {
+            Lifecycle.State.DESTROYED -> {}
+            Lifecycle.State.INITIALIZED -> {}
+            Lifecycle.State.CREATED -> {}
+            Lifecycle.State.STARTED -> {}
+            Lifecycle.State.RESUMED -> {
+                viewModel.loadData()
+            }
+        }
+    }
+
     fun getMonthBudget(): String {
         return monthTackerInfoState.budget.money() + "VND"
     }
